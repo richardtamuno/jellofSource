6c810dc832639b12088f053691a6e029236e287b
Rtams <richardtamunotonye@gmail.com>
2017-10-11 16:57:01 +0200

    Stalker Solution

---
+++ jsStalker/script.js
@@ -0,0 +1,4 @@
+var fName = prompt("Please enter your first name");
+var lName = prompt("please enter your surname");
+var age = prompt("please enter your age");
+alert("Hello" + fName + "" + lName + "I am amazed you are " + age + " years old.");
